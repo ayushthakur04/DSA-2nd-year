b75a06fc5ec7efd93d1be2665ba6bd3bb2bf12bc
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-01-08 15:46:22 +0530

    Create Guess number higher or lower.cpp

---
+++ Assignment  7/Guess number higher or lower.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l=1,r=n;
+      
+      
+        while(l<=n){
+             int mid=l+(r-l)/2;
+              int g=guess(mid);
+            if(g==0){
+                return mid;
+            }else if(g==1){
+                l = mid+1;
+            }else{
+                 r=mid-1;
+            }
+       }
+         return 0;
+
+    }
+};
