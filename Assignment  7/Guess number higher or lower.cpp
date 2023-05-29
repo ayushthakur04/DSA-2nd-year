f8d4e45fd9858e58b90800fa2974cb59375cf0e7
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-05-29 23:43:41 +0530

    Guess number higher or lower.cpp

--- Assignment  7/Guess number higher or lower.cpp
+++ Assignment  7/Guess number higher or lower.cpp
@@ -6,7 +6,7 @@ public:
       
         while(l<=n){
              int mid=l+(r-l)/2;
-              int g=guess(mid);
+              int g=guess(mid);a
             if(g==0){
                 return mid;
             }else if(g==1){
